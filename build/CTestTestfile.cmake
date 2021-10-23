ab62b3a16a6ae84b417d0e9770353c6f17c32436
Sachin Iyer <sachinjiyer@gmail.com>
2021-10-22 22:21:18 -0700

    get rid of build and ccls stuff

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/siyer/docs/projects/ongoing/corelink/circular_buffer
-# Build directory: /home/siyer/docs/projects/ongoing/corelink/circular_buffer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/siyer/docs/projects/ongoing/corelink/circular_buffer/build/circular_buffer_test_include-b12d07c.cmake")
